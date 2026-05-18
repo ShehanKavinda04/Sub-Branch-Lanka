87014f0a6ab3421acd9593751ca19e84895423f5
skavinda <kavinda20000602@gmail.com>
2026-05-18 22:47:02 +0530

    implement CartPage component to display cart items and order summary

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { useCart } from "../Context/CartContext";
 import { useNavigate } from "react-router-dom";
 import { FaTrash, FaShoppingBag } from "react-icons/fa";
